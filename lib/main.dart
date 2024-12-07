0c0c9c6c7d617ebc85d71515e3b4c5dbb5dbe740
Ark1ight <benoi1.suy@gmail.com>
2024-12-07 16:14:32 +0100

    added route logic in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:crown/shared/services/local_posts_data_source/fake_local_posts_data_source.dart';
 import 'package:crown/shared/services/posts_data_source/posts_fake_data_source.dart';
 import 'package:crown/shared/services/posts_repository/posts_repository.dart';
+import 'package:crown/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,7 +14,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return RepositoryProvider(
@@ -22,16 +22,24 @@ class MyApp extends StatelessWidget {
         localPostsDataSource: FakeLocalPostsDataSource(),
       ),
       child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const HomePage(),
+        debugShowCheckedModeBanner: false,
         routes: {
-          '/second': (context) => const HomePage(),
+          '/': (context) => const SplashScreen(),
+          '/home': (context) => const HomePage(),
+        },
+        onGenerateRoute: (routeSettings){
+          Widget screen = Container(color: Colors.pink);
+          final argument = routeSettings.arguments;
+          switch(routeSettings.name){
+            case '/second':
+              screen = Container(color: Colors.blue);
+              break;
+          }
+          return MaterialPageRoute(builder: (context) => screen);
         },
       ),
     );
   }
 }
+
 
---
+++ lib/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const HomePage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.grade_rounded, size: 100, color: Colors.yellow),
+            SizedBox(height: 20),
+            Text(
+              'Crown',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
