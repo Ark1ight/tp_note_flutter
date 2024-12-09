cbf79d1c365c23172836e82c47d1e58965a2a48b
Ark1ight <benoi1.suy@gmail.com>
2024-12-09 15:48:04 +0100

    refactored and corrected errors in bloc and creation form

--- lib/screens/post_creation_screen/widgets/post_creation_form.dart
+++ lib/screens/post_creation_screen/widgets/post_creation_form.dart
@@ -16,6 +16,7 @@ class _PostCreationFormState extends State<PostCreationForm> {
   final _formKey = GlobalKey<FormState>();
   final _titleController = TextEditingController();
   final _descriptionController = TextEditingController();
+  bool _buttonPressed = false;
 
   @override
   void dispose() {
@@ -26,56 +27,80 @@ class _PostCreationFormState extends State<PostCreationForm> {
 
   void _submitForm() {
     if (_formKey.currentState!.validate()) {
+      setState(() {
+        _buttonPressed = true;
+      });
 
       final title = _titleController.text;
       final description = _descriptionController.text;
       _addPost(title, description);
-
-      HomePageScreen.navigateTo(context);
     }
+  }
+
+  void _addPost(String title, String description) {
+    final postDetailBloc = context.read<PostDetailBloc>();
+    postDetailBloc.add(AddPost(PostDTO(title: title, description: description)));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextFormField(
-              controller: _titleController,
-              decoration: const InputDecoration(labelText: 'Title'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter the title of your thought';
-                }
-                return null;
-              },
+    return BlocListener<PostDetailBloc, PostDetailState>(
+      listener: (context, state) {
+        if (state.status == PostDetailStatus.success) {
+          HomePageScreen.navigateTo(context);
+        }
+      },
+      child: BlocBuilder<PostDetailBloc, PostDetailState>(
+        builder: (context, state) {
+          return Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _titleController,
+                    decoration: const InputDecoration(labelText: 'Title'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the title of your thought';
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    controller: _descriptionController,
+                    decoration: const InputDecoration(labelText: 'Description'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a description of your thought';
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _submitForm,
+                    child: const Text('Submit'),
+                  ),
+                  if (_buttonPressed)
+                    switch (state.status) {
+                      PostDetailStatus.loading => const Padding(
+                        padding: EdgeInsets.only(top: 20),
+                        child: CircularProgressIndicator(),
+                      ),
+                      PostDetailStatus.success => const Padding(
+                        padding: EdgeInsets.only(top: 20),
+                        child: Icon(Icons.check, color: Colors.green),
+                      ),
+                      _ => const SizedBox.shrink(),
+                    },
+                ],
+              ),
             ),
-            TextFormField(
-              controller: _descriptionController,
-              decoration: const InputDecoration(labelText: 'Description'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter a description of your thought';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _submitForm,
-              child: const Text('Submit'),
-            ),
-          ],
-        ),
+          );
+        },
       ),
     );
-  }
-
-  void _addPost(String title, String description) {
-    final postsBloc = BlocProvider.of<PostDetailBloc>(context);
-    postsBloc.add(AddPost(PostDTO(title: title, description: description)));
   }
 }
